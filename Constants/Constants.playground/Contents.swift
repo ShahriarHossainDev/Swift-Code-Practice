9bec82babc1caa8d2bb5a8a2a52c14ac454ccf6a
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2021-08-26 21:40:46 +0600

    constants

---
+++ Constants/Constants.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//Constants Declaration
+let constA = 42
+print(constA)
+//Constants are treated just like regular variables except the fact that their values cannot be modified after their definition.
+
+//Naming Constants
+let _const = "Hello, Swift 4!"
+print(_const)
+
+let 你好 = "你好世界"
+print(你好)
+
+//Printing Constants
+let constAA = "Godzilla"
+let constB = 1000.00
+
+print("Value of \(constAA) is more than \(constB) millions")
